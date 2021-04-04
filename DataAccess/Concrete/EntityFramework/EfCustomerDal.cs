76286f35c8f0d85d024f555f76dc5857ba57408a
yasin çınar <yasin001905@gmail.com>
2021-04-05 01:53:54 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/EfCustomerDal.cs
@@ -0,0 +1,31 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCustomerDal : EfEntityRepositoryBase<Customer, CarRentalContext>, ICustomerDal
+    {
+
+
+        public bool DeleteCustomerIfNotReturnDateNull(Customer customer)
+        {
+            using (CarRentalContext context = new CarRentalContext())
+            {
+                var find = context.Rentals.Any(i => i.CustomerId == customer.Id && i.ReturnDate == null);
+                if (!find)
+                {
+                    context.Remove(customer);
+                    context.SaveChanges();
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
